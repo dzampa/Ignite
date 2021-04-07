28a4bdb4e692d9948ad496d0cd21ab9b56f52dea
Diego Zamparo <dzamparo@gmail.com>
2021-04-06 23:20:29 -0300

    code refactoring

--- NodeJs/RENTALX/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUserCase.spec.ts
+++ NodeJs/RENTALX/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUserCase.spec.ts
@@ -1,6 +1,6 @@
-import { AppError } from "@errors/AppError";
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositotyInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateUserUserCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUserCase } from "./AuthenticateUserUserCase";
