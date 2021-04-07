abf6e6347d1b8e6c0c5a0a1ec77dcedefe31898c
Diego Zamparo <dzamparo@gmail.com>
2021-04-06 21:54:47 -0300

    add authenticateUser test

---
+++ NodeJs/RENTALX/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUserCase.spec.ts
@@ -0,0 +1,60 @@
+import { AppError } from "../../../../erros/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositotyInMemory";
+import { CreateUserUserCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUserCase } from "./AuthenticateUserUserCase";
+
+let authenticateUserUseCase: AuthenticateUserUserCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createuserUseCase: CreateUserUserCase;
+
+describe("Authenticate User", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        authenticateUserUseCase = new AuthenticateUserUserCase(
+            usersRepositoryInMemory
+        );
+        createuserUseCase = new CreateUserUserCase(usersRepositoryInMemory);
+    });
+    it("should be able to authenticate an user", async () => {
+        const user: ICreateUserDTO = {
+            driver_license: "000123",
+            email: "user@teste.com",
+            password: "1234",
+            name: "User Test",
+        };
+        await createuserUseCase.execute(user);
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password,
+        });
+        expect(result).toHaveProperty("token");
+    });
+
+    it("should not be able to authenticate an non existent user", () => {
+        expect(async () => {
+            await authenticateUserUseCase.execute({
+                email: "false@email.com",
+                password: "1234",
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to authneticate with incorrect passoword", () => {
+        expect(async () => {
+            const user: ICreateUserDTO = {
+                driver_license: "9999",
+                email: "user@user.com",
+                password: "1234",
+                name: "User Test Error",
+            };
+
+            await createuserUseCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "incorrect password",
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
